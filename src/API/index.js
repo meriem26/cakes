c8866124302956057850e8a974efba46c619ec16
Meriem <mmlys89@gmail.com>
2019-11-10 14:29:06 +0000

    Use axios to fetch data

--- src/API/index.js
+++ src/API/index.js
@@ -1,9 +1,8 @@
+import axios from 'axios';
 export const fetchCakesList = async () => {
-  const res = await fetch(
-    "https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/raw/8dd19a88f9b8d24c23d9960f3300d0c917a4f07c/cake.json"
-  );
-  const data = await res.json();
-  return data;
-};
+	const res = await axios.get(
+		'https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/raw/8dd19a88f9b8d24c23d9960f3300d0c917a4f07c/cake.json'
+	);
 
-// promise
+	return res.data;
+};
