7de95493f548b9fa58eb5ee58923d479e6d5bda9
Meriem <mmlys89@gmail.com>
2019-11-10 11:29:17 +0000

    Create search method

--- src/Components/CakesList.js
+++ src/Components/CakesList.js
@@ -1,44 +1,65 @@
-import React from "react";
-import CardInfo from "./CardInfo";
-import { fetchCakesList } from "../API/index";
+import React from 'react';
+import CardInfo from './CardInfo';
+import { fetchCakesList } from '../API/index';
+//import CakesSearch from './CakesSearch';
 class CakesList extends React.Component {
-  state = {
-    cakes: [],
-    error: null
-  };
+	state = {
+		cakes: [],
+		filterCakeList: [],
+		error: null,
+	};
 
-  async componentDidMount() {
-    try {
-      const cakes = await fetchCakesList();
-      this.setState({
-        cakes
-      });
-    } catch (err) {
-      console.error(err);
-      this.setState({
-        error: true
-      });
-    }
-  }
-  render() {
-    const { cakes, error } = this.state;
+	// https://reactjs.org/docs/lifting-state-up.html
 
-    return (
-      <div>
-        {error ? (
-          <h1>You have an Error </h1>
-        ) : (
-          <div>
-            <h1>Cakes List</h1>
-            <ul>
-              {cakes.map((cake, index) => {
-                return <CardInfo key={index} cake={cake} />;
-              })}
-            </ul>
-          </div>
-        )}
-      </div>
-    );
-  }
+	async componentDidMount() {
+		try {
+			const cakes = await fetchCakesList();
+			this.setState({
+				cakes,
+				filterCakeList: cakes,
+			});
+		} catch (err) {
+			console.error(err);
+			this.setState({
+				error: true,
+			});
+		}
+	}
+
+	doSearch = e => {
+		const { cakes = [] } = this.state;
+		const { value } = e.target;
+
+		const filterCakeList = cakes.filter(cake => {
+			const { title = '' } = cake;
+			return title.toLowerCase().includes(value);
+		});
+
+		this.setState({
+			filterCakeList,
+		});
+	};
+
+	render() {
+		const { filterCakeList, error } = this.state;
+		return (
+			<div>
+				{error ? (
+					<h1>You have an Error </h1>
+				) : (
+					<div>
+						<h1>Cakes List</h1>
+						{/* <CakesSearch value={this.state.cakeSearch} handleChange={this.doSearch} /> */}
+						<input type="text" onChange={this.doSearch} placeholder="Search..." />
+						<ul>
+							{filterCakeList.map((cake, index) => {
+								return <CardInfo key={index} cake={cake} />;
+							})}
+						</ul>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
 export default CakesList;
